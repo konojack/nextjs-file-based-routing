75074b4b469289280966f6ffde5b9334bba25561
konojack <konojack@gmail.com>
2022-12-12 14:13:24 +0100

    added styles for events components

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import classes from './EventItem.module.css';
+
 function EventItem({ event: { title, image, date, location, id } }) {
   const readableDate = new Date(date).toLocaleDateString('en-US', {
     day: 'numeric',
@@ -12,19 +14,19 @@ function EventItem({ event: { title, image, date, location, id } }) {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <Image src={`/${image}`} alt="" width="500" height="500" />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{readableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
--- components/events/EventList.js
+++ components/events/EventList.js
@@ -1,8 +1,9 @@
 import EventItem from './EventItem';
+import classes from './EventList.module.css';
 
 function EventList({ events }) {
   return (
-    <ul>
+    <ul className={classes.list}>
       {events.map(event => (
         <EventItem key={event.id} event={event} />
       ))}
