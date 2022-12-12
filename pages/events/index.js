abbd6cbf111470dd548ecfe8c65456e3207fbba9
konojack <konojack@gmail.com>
2022-12-12 18:13:58 +0100

    events search component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,12 @@
 import EventList from '../../components/events/EventList';
+import EventsSearch from '../../components/events/EventsSearch';
 import { getAllEvents } from '../../data/dummy-data';
 
 export default function EventsPage() {
   const events = getAllEvents();
   return (
     <div>
+      <EventsSearch />
       <EventList events={events} />
     </div>
   );
