8302a1551c315ec429889f42467c2a2e8d2d45c6
konojack <konojack@gmail.com>
2022-12-12 17:00:51 +0100

    added buttons & icons

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,6 +1,8 @@
 import Image from 'next/image';
-import Link from 'next/link';
-
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 import classes from './EventItem.module.css';
 
 function EventItem({ event: { title, image, date, location, id } }) {
@@ -20,14 +22,21 @@ function EventItem({ event: { title, image, date, location, id } }) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{readableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
---
+++ components/ui/button.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import classes from './button.module.css';
+
+function Button({ children, link }) {
+  return (
+    <Link className={classes.btn} href={link}>
+      {children}
+    </Link>
+  );
+}
+
+export default Button;
