f563b256cd305f506f024caa1e808814213b2c0f
konojack <konojack@gmail.com>
2022-12-12 18:57:39 +0100

    slug page for events

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,3 +1,36 @@
+import { useRouter } from 'next/router';
+import { getFilteredEvents } from '../../data/dummy-data';
+
 export default function EventSlugPage() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2022 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+  if (!filterData || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter</p>;
+  }
   return <div>EventSlug Page</div>;
 }
