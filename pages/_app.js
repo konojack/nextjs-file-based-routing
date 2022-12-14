1947d3967dc7fa16143db6d0fca5f1c0802ed574
konojack <konojack@gmail.com>
2022-12-14 16:09:01 +0100

    added title

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
+import Head from 'next/head';
 import Layout from '../components/layout/Layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Events App</title>
+        <meta
+          name="description"
+          content="This is an event app! Browse your event!"
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
