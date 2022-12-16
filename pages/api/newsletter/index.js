b1d8d8e1dcd8e90da0b691b93729cef84d48b99f
konojack <konojack@gmail.com>
2022-12-16 12:30:47 +0100

    connected to MongoDB to store newsletter emails

--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -1,10 +1,11 @@
 import Joi from 'joi';
+import { MongoClient } from 'mongodb';
 
 const schema = Joi.object({
   email: Joi.string().email().required(),
 });
 
-const handler = (req, res) => {
+const handler = async (req, res) => {
   if (req.method == 'POST') {
     const { error } = schema.validate(req.body);
 
@@ -13,6 +14,13 @@ const handler = (req, res) => {
     }
 
     const email = req.body.email;
+
+    const client = await MongoClient.connect(process.env.MONGODB_URL);
+    const db = client.db();
+    await db.collection('emails').insertOne({ email: email });
+
+    client.close();
+
     res.status(201).json({ success: true, email });
   }
 };
