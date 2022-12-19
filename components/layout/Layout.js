100803487a09e755ee089dd4b6b82cbf23a62d5c
konojack <konojack@gmail.com>
2022-12-20 00:57:16 +0100

    added notification based on the context to the Layout component

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,23 @@
+import { useContext } from 'react';
+import { NotificationContext } from '../../store/notification-context';
+import Notification from '../ui/notification';
 import MainHeader from './MainHeader';
 
 const Layout = ({ children }) => {
+  const notificationCtx = useContext(NotificationContext);
+
+  const { notification: activeNotification } = notificationCtx;
   return (
     <>
       <MainHeader />
       <main>{children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification?.title}
+          message={activeNotification?.message}
+          status={activeNotification?.status}
+        />
+      )}
     </>
   );
 };
