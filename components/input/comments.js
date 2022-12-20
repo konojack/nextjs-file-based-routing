ec6eaa3f8c9df714c44f5fd4a20b34fc65881963
konojack <konojack@gmail.com>
2022-12-20 02:09:46 +0100

    added comments notification + loading comments SVG

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,18 +1,25 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
+import Image from 'next/image';
+import { NotificationContext } from '../../store/notification-context';
 
 function Comments({ eventId }) {
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const [loadingComments, setLoadingComments] = useState(false);
+
+  const notificationCtx = useContext(NotificationContext);
 
   const getComments = useCallback(() => {
+    setLoadingComments(true);
     fetch(`/api/comments/${eventId}`)
       .then(resp => resp.json())
       .then(resp => {
         setComments(resp.comments);
+        setLoadingComments(false);
       });
   }, [eventId]);
 
@@ -27,6 +34,12 @@ function Comments({ eventId }) {
   }
 
   function addCommentHandler(commentData) {
+    notificationCtx.showNotification({
+      title: 'Sending...',
+      message: 'Your comment is saving',
+      status: 'pending',
+    });
+
     fetch(`/api/comments/${eventId}`, {
       method: 'POST',
       body: JSON.stringify(commentData),
@@ -34,8 +47,31 @@ function Comments({ eventId }) {
         'Content-Type': 'application/json',
       },
     })
-      .then(resp => resp.json())
-      .then(resp => getComments());
+      .then(resp => {
+        if (resp.ok) {
+          return resp.json();
+        }
+
+        return resp.json().then(resp => {
+          throw new Error(resp.message || 'Something went wrong!');
+        });
+      })
+      .then(resp => {
+        notificationCtx.showNotification({
+          title: 'Success!',
+          message: 'Your comment has been saved',
+          status: 'success',
+        });
+
+        getComments();
+      })
+      .catch(err => {
+        notificationCtx.showNotification({
+          title: 'Error!',
+          message: err.message || 'Something went wrong!',
+          status: 'error',
+        });
+      });
   }
 
   return (
@@ -44,7 +80,16 @@ function Comments({ eventId }) {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList comments={comments} />}
+      {showComments && loadingComments && (
+        <Image
+          src="/spinner.svg"
+          className={classes.spinner}
+          alt="vercel"
+          height="100"
+          width="100"
+        />
+      )}
+      {showComments && !loadingComments && <CommentList comments={comments} />}
     </section>
   );
 }
